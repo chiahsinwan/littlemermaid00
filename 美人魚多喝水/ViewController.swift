ded55c31d0f4051f6065cb857ccd31fd60bc3a44
李佳昕 <ChiaHsinLee@f0036.f1.ntu.edu.tw>
2017-03-05 20:38:22 +0800

    Initial Commit

---
+++ 美人魚多喝水/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  美人魚多喝水
+//
+//  Created by 李佳昕 on 2017/3/5.
+//  Copyright © 2017年 李佳昕. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
